7c4cb44e2853a922b697f48778b24ab5f972c604
Shirley Park <121600587+shirley-park@users.noreply.github.com>
2023-10-03 15:32:27 +1300

    variable declarations

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
-
 func main() {
-	fmt.Println("Hello!")
+	/* VARIABLE DECLARATIONS
+	var card string = "Ace of Spades" -> long form way of declaring variables
+	card := "Ace of Spades" -> Go can figure out that card is a string
+	card = "Five of Hearts" -> Reassigning card to "Five of Hearts"
+
+	fmt.Println(card)
+	*/
+
 }
