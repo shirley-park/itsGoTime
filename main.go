9d99b49d4cf7048cdf623850497dc4d584459907
Shirley Park <121600587+shirley-park@users.noreply.github.com>
2023-10-04 13:57:01 +1300

    functions and return types

--- main.go
+++ main.go
@@ -1,12 +1,27 @@
 package main
 
+import "fmt"
+
 func main() {
 	/* VARIABLE DECLARATIONS
+	------------------------
 	var card string = "Ace of Spades" -> long form way of declaring variables
 	card := "Ace of Spades" -> Go can figure out that card is a string
 	card = "Five of Hearts" -> Reassigning card to "Five of Hearts"
-
-	fmt.Println(card)
 	*/
 
+	/* FUNCTIONS AND RETURN TYPES
+	instead of declaring a var, calling a function that will return the card
+	------------------------
+	*/
+	card := newCard()
+
+	fmt.Println(card)
+
+}
+
+// defining a separate function that is called from main().
+// whenever newCard() is called, it will return the card value of type string
+func newCard() string {
+	return "test"
 }
