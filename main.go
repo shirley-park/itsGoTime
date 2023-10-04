24b2faf04bcc500cadab6cb9cf3fa357ce5649d7
Shirley Park <121600587+shirley-park@users.noreply.github.com>
2023-10-04 14:12:35 +1300

    slices and for loops

--- main.go
+++ main.go
@@ -4,24 +4,50 @@ import "fmt"
 
 func main() {
 	/* VARIABLE DECLARATIONS
-	------------------------
+
 	var card string = "Ace of Spades" -> long form way of declaring variables
 	card := "Ace of Spades" -> Go can figure out that card is a string
 	card = "Five of Hearts" -> Reassigning card to "Five of Hearts"
+	fmt.Println(card)
 	*/
+
+	// ------------------------
 
 	/* FUNCTIONS AND RETURN TYPES
 	instead of declaring a var, calling a function that will return the card
-	------------------------
-	*/
-	card := newCard()
 
+	card := newCard()
 	fmt.Println(card)
+	*/
+
+	// ------------------------
+
+	/* SLICES AND FOR LOOPS
+	Go has two basic data structures for handling lists of records
+	> Array:
+		- fixed length list of things
+	> Slice:
+		- An array that can grow or shrink, can add and subtract new cards out of slice
+		- Every element in a slice must be of same type (ie. all string or int)
+
+	*/
+
+	cards := []string{newCard(), newCard()}
+	cards = append(cards, "Six of Spades")
+	// note: append() does not modify the existing slice
+	// instead returns a new slice which we assign back to var cards
+
+	// how do we iterate over the cards slice?
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
+
+	fmt.Println(cards)
 
 }
 
 // defining a separate function that is called from main().
 // whenever newCard() is called, it will return the card value of type string
 func newCard() string {
-	return "test"
+	return "Five of Diamonds"
 }
